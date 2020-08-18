86c1432a161c325027bf6918ce87346c13d3206c
Aubrey Smith <2274500+yaodema@users.noreply.github.com>
2020-08-18 02:48:14 -0600

    Implement getAuthType() and getConfig()
    Authorization functions moved to getConfig.
    Community Connectors have limited authorization options
    when compared to Google-made connectors. KEY AuthType
    includes only a token, and we need to point the connector
    at a custom URL per user / data source.

--- src/main.js
+++ src/main.js
@@ -1,4 +1,5 @@
 // HarperDB Data Studio Connector
+// main.js: main program components for the Community Connector
 // Contributor(s): Aubrey Smith
 
 function getAuthType() {
@@ -17,17 +18,103 @@ function getConfig(request) {
 	// the first step is always to get the user's URL for connecting to the database, and
 	//  their Basic Authentication Key. This needs to be checked before proceeding with
 	//  the rest of configuration.
-	// two versions of configuration will exist, one with everything past authentication
-	//  on one page, and one that instead uses two more pages: the first for choosing
-	//  whether to use a Schema.Table pair or to take a raw SQL SELECT query, and the
-	//  second to get that table or query from the user.
-	// The URL where the HarperDB instance resides should be given with port information,
-	//  and must be given such a port if that instance usesa non-standard port. If no port
-	//  is provided, the connector should test these ports in order:
-	//    31283 (HarperDB HTTPS),
-	//    443 (HTTPS),
-	//    9925 (HarperDB HTTP),
-	//    80 (HTTP).
+	// we may produce multiple versions of this configuration, but for now we're going for
+	//  the fewest pages possible. we'll see which method flows best, and go with that one
+	//  in the end.
+	
+	var cc = DataStudioApp.createCommunityConnector();
+	var config = cc.getConfig();
+	
+	// first page info must always be included, because that enables params to be sent
+	// to the next step correctly.
+	
+	// BUILD first page here!
+	var cfgUrl = config.newTextInput()
+		.setID("url")
+		.setName("Web URL")
+		.setHelpText("The URL of your HarperDB instance's API gateway")
+	//	.setPlaceholder("this should contain a trailing https url for the HarperDB cloud")
+		.setIsDynamic(true);
+	var cfgKey = config.newTextInput()
+		.setID("key")
+		.setName("Basic Auth Key")
+		.setHelpText("The Basic Authorization Key for your read access to the DB")
+		.setPlaceholder("r9UzYUgX99PRpWcp50t4t8AAFA==") // random data, not a usable key
+		.setIsDynamic(true);
+	var cfgQueryType = config.newSelectSingle()
+		.setID("queryType")
+		.setName("Query Type")
+		.setHelpText("Either Schema.Table for simple SELECT * on that table, or SQL for freeform")
+		.addOption(config.newOptionBuilder()
+			.setLabel("SQL")
+			.setValue("SQL"))
+		.addOption(config.newOptionBuilder()
+			.setLabel("Table")
+			.setValue("TABLE"));
+		.setIsDynamic(true);
+	var cfgSecure = config.newCheckbox()
+		.setID("secure")
+		.setName("Secure Connections Only?")
+		.setHelpText("If checked, only HTTPS connections will be made to the server.")
+		.setIsDynamic(true);
+	var cfgBadCert = config.newCheckbox()
+		.setID("badCert")
+		.setName("Allow Bad Certs?")
+		.setHelpText("If checked, HTTPS connections will work even for unverifiable certificates.")
+		.setIsDynamic(true);
+	
+	if(!('configParams' in request) || !request.configParams.queryType) {
+		// first page, nothing more to build.
+		// second check above is in case the user changed something that wiped out
+		//  the query type.
+		config.setIsSteppedConfig(true);
+		// reset UserProperties for this data source; they will be set on auth.
+		var userp = PropertiesService.getUserProperties();
+		userp.deleteAllProperties();
+		// send config to the UI.
+		return config.build();
+	} else {
+		// test that user authorization is working!
+		var cfgp = request.configParams;
+		var schemasJson = hdbSqlQuery("SHOW SCHEMAS",cfgp);
+		// if the above function throws an error, the error will flow through to GDS.
+		
+		// if the authorization works, use the response to queryType to determine which
+		//  page we display to the user.
+		if(cfgp.queryType == "SQL") {
+			// add a field for SQL query
+			var cfgSql = config.newTextInput()
+				.setID("sql")
+				.setName("SQL Query")
+				.setHelpText("The SQL Query to HarperDB")
+				.setPlaceholder("SELECT ...");
+			config.setIsSteppedConfig(false);
+			return config.build();
+		} else if(cfgp.queryType == "TABLE") {
+			// add two fields, one for Schema and one for Table.
+			var cfgSchema = config.newTextInput()
+				.setID("schema")
+				.setName("Schema")
+				.setHelpText("The HarperDB Schema (not Data Studio Schema)")
+				.setPlaceholder("dev");
+			// Q: should we offer a drop-down Schema instead of text entry?
+			var cfgTable = config.newTextInput()
+				.setID("table")
+				.setName("Table")
+				.setHelpText("The Table to SELECT * from in this Schema");
+			// Q: should we make an additional step to configuration here, where Schema
+			//  choice determines which one we Describe Schema from.
+			//  (this would use the describe_schema operation rather than an SQL query)
+			config.setIsSteppedConfig(false);
+			return config.build();
+		} else {
+			// illegal queryType!
+			cc.newUserError()
+				.setText("Illegal Query Type! Type must be SQL or Schema.Table.")
+				.setDebugText("Received queryType = " + cfgp.queryType)
+				.throwException();
+		}
+	}
 	
 	// stub
 }
@@ -41,6 +128,10 @@ function getSchema(request) {
 	// Note that complex data types need to be flattened for the schema, so the rows that
 	//  the SQL query produces may not match the names and number of fields in the GDS
 	//  version of the schema.
+	
+	// all non-Number fields are Dimensions.
+	// the id (hash) field of any table is a Dimension.
+	// any non-id Number field is a Metric, and should be set in Sum mode by default.
 	
 	// stub
 }
